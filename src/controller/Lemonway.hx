0df03e156c825ce42b2c0c2845e2d2d8477f112c
Unknown <antoine@zulke.net>
2018-02-13 10:49:28 -0500

    Lemonway - WIP

--- src/controller/Lemonway.hx
+++ src/controller/Lemonway.hx
@@ -17,21 +17,31 @@ class Lemonway extends Controller
 
     @tpl("lemonway/default.mtt")
 	function doDefault() {
-        var req = new haxe.Http("http://api.coindesk.com/v1/bpi/currentprice.json");
-        req.onData = function(data) {
-            this.hop += " - data: " + data;
-            var json = haxe.Json.parse(data);
-            throw Ok('/', "Data recues:"+data);
-        }
-        req.onError = function(error) {
-            this.hop += " - error: " + error;
-            throw Ok('/', "Error:"+error);
-        }
-        // req.onStatus = function(status) {
-        //     this.hop += " - status: " + status;
-        //     throw Ok('/', "Status:"+status);
+        var now = Date.now();
+
+		//correct the time to get the local time, by using the correction in the config file
+		//var now2 = DateTools.delta(now, DateTools.hours(Std.parseFloat(App.config.get("nowdate_correction"))));
+
+		throw Ok('/messages', "Server date now: " + now.toString());
+
+
+
+
+        // var req = new haxe.Http("http://api.coindesk.com/v1/bpi/currentprice.json");
+        // req.onData = function(data) {
+        //     this.hop += " - data: " + data;
+        //     var json = haxe.Json.parse(data);
+        //     throw Ok('/', "Data recues:"+data);
         // }
-        req.request(false);
+        // req.onError = function(error) {
+        //     this.hop += " - error: " + error;
+        //     throw Ok('/', "Error:"+error);
+        // }
+        // // req.onStatus = function(status) {
+        // //     this.hop += " - status: " + status;
+        // //     throw Ok('/', "Status:"+status);
+        // // }
+        // req.request(false);
     }
     
 }
